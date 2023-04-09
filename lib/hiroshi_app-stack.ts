9ef3498d0cd81909fb64cfa1963d2865cbbb5b63
Hiro <41129335+hiroshiuk1991@users.noreply.github.com>
2023-04-09 23:45:13 +0100

    feat: added user pool logic in stack

--- lib/hiroshi_app-stack.ts
+++ lib/hiroshi_app-stack.ts
@@ -8,6 +8,37 @@ export class HiroshiAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // create a new user pool
+    const userPool = new cognito.UserPool(this, "MyUserPool", {
+      userPoolName: " my-user-pool",
+      selfSignUpEnabled: true,
+      autoVerify: { email: true },
+      signInAliases: { email: true },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireDigits: true,
+        requireSymbols: false,
+        requireUppercase: true,
+      },
+    });
+    // create a user pool client
+    const userPoolClient = new cognito.userPoolClient(
+      this,
+      "MyUSerPoolClient",
+      {
+        userPool,
+        userPoolClientName: "my-user-pool-client",
+        generateSecret: false,
+      }
+    );
+
+    // create an output for the user pool client ID
+    new cdk.CfnOutput(this, "UserPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+      exportName: `${this.stackName}-UserPoolClientID`,
+    });
+
     // The code that defines your stack goes here
 
     // example resource
