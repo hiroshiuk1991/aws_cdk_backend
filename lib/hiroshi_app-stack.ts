89b82d4a70c31fcf5333fb9bb81073edf260e8f1
Hiro <41129335+hiroshiuk1991@users.noreply.github.com>
2023-04-11 23:03:26 +0100

    rewrote userPool logic in stack

--- lib/hiroshi_app-stack.ts
+++ lib/hiroshi_app-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+
 import * as cognito from "@aws-cdk/aws-cognito";
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -8,35 +9,19 @@ export class HiroshiAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // create a new user pool
-    const userPool = new cognito.UserPool(this, "MyUserPool", {
-      userPoolName: " my-user-pool",
+    const userPool = new cognito.UserPool(this, "UserPool", {
       selfSignUpEnabled: true,
-      autoVerify: { email: true },
       signInAliases: { email: true },
-      passwordPolicy: {
-        minLength: 8,
-        requireLowercase: true,
-        requireDigits: true,
-        requireSymbols: false,
-        requireUppercase: true,
-      },
     });
-    // create a user pool client
-    const userPoolClient = new cognito.userPoolClient(
-      this,
-      "MyUSerPoolClient",
-      {
-        userPool,
-        userPoolClientName: "my-user-pool-client",
-        generateSecret: false,
-      }
-    );
 
-    // create an output for the user pool client ID
-    new cdk.CfnOutput(this, "UserPoolClientId", {
-      value: userPoolClient.userPoolClientId,
-      exportName: `${this.stackName}-UserPoolClientID`,
+    const identityPool = new cognito.CfnIdentityPool(this, "IdentityPool", {
+      allowUnauthenticatedIdentities: false,
+      cognitoIdentityProviders: [
+        {
+          clientId: userPool.UserPoolClientId,
+          providerName: userPool.userPoolProviderName,
+        },
+      ],
     });
 
     // The code that defines your stack goes here
